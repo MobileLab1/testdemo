c0e23e023276781197f8dc46a352b071e9fa382a
mladmin <cognizantmobilelab1@icloud.com>
2018-02-02 10:56:15 -0700

    ch1

--- app/src/main/java/com/experitest/ExperiBank/LoginActivity.java
+++ app/src/main/java/com/experitest/ExperiBank/LoginActivity.java
@@ -21,7 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 //import android.widget.Toast;
-//test
+
 
 public class LoginActivity extends Activity {
 	/** Called when the activity is first created. */
